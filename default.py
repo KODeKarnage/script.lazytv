885860053da644145db751ea7f0bbfbe7e376634
KODeKarnage <sublime.kev@gmail.com>
2014-03-25 23:28:36 +1100

    frodo setting migration function

--- default.py
+++ default.py
@@ -38,6 +38,8 @@ add_this_movie   = {'jsonrpc': '2.0','id': 1, "method": 'Playlist.Add', 				"par
 get_movies       = {"jsonrpc": "2.0",'id': 1, "method": "VideoLibrary.GetMovies", 	"params": { "filter": {"field": "playcount", "operator": "is", "value": "0"}, "properties" : ["playcount", "title"] }}
 get_moviesw      = {"jsonrpc": "2.0",'id': 1, "method": "VideoLibrary.GetMovies", 	"params": { "filter": {"field": "playcount", "operator": "is", "value": "1"}, "properties" : ["playcount", "title"] }}
 get_moviesa      = {"jsonrpc": "2.0",'id': 1, "method": "VideoLibrary.GetMovies", 	"params": { "properties" : ["playcount", "title"] }}
+get_legacy		 = {"jsonrpc": "2.0","id": 1, "method": "VideoLibrary.GetTVShows",  "params": { "properties" : ["mpaa","genre"] }}
+
 
 __addon__        = xbmcaddon.Addon()
 __addonid__      = __addon__.getAddonInfo('id')
@@ -123,7 +125,7 @@ def json_query(query, ret):
 		result = xbmc.executeJSONRPC(xbmc_request)
 		#print result
 		#result = unicode(result, 'utf-8', errors='ignore')
-		log('result = ' + str(result))
+		#log('result = ' + str(result))
 		if ret:
 			return json.loads(result)['result']
 		else:
@@ -712,6 +714,65 @@ def main_entry():
 		create_next_episode_list(population)
 
 
+def convert_previous_settings(ignore):
+	filter_genre         = True if __setting__('filter_genre') == 'true' else False
+	filter_length         = True if __setting__('filter_length') == 'true' else False
+	filter_rating         = True if __setting__('filter_rating') == 'true' else False
+	filter_show         = True if __setting__('filter_show') == 'true' else False
+
+	# convert the ignore list into a dict
+	jkl = {}
+	all_showids = []
+	for ig in ignore.split("|"):
+		if ig:
+			k, v = ig.split(":-:")
+			if k in jkl:
+				jkl[k].append(v)
+			else:
+				jkl[k] = [v]
+	if jkl:
+		# create a list of permissable TV shows
+		show_data = json_query(get_legacy, True)
+
+		if 'tvshows' in show_data and show_data['tvshows']:
+			show_data = show_data['tvshows']
+	
+			all_showids = [x['tvshowid'] for x in show_data]
+
+			for show in show_data:
+
+				if filter_genre and "genre" in jkl:
+					for gen in jkl['genre']:
+						if gen in show['genre'] and show['tvshowid'] in all_showids:
+							all_showids.remove(show['tvshowid'])
+
+				if filter_rating and "rating" in jkl:
+					if show['mpaa'] in jkl['rating'] and show['tvshowid'] in all_showids:
+						all_showids.remove(show['tvshowid'])
+
+				if filter_show and "name" in jkl:
+
+					if str(show['tvshowid']) in jkl['name'] and show['tvshowid'] in all_showids:
+						all_showids.remove(show['tvshowid'])
+	if all_showids:
+		spec_shows = [int(x) for x in all_showids]
+
+		# save the new list to settings
+		__addon__.setSetting('selection',str(spec_shows))
+		# set the filterYN to be Y
+		__addon__.setSetting('filterYN','true')
+		filterYN = True
+
+
+	# reset IGNORE to be null
+	__addon__.setSetting('IGNORE','')
+
+
+
+# this check is to ensure that the Ignore list from the previous addon is respected and replaced in the new version
+ignore = __setting__('IGNORE')
+if ignore:
+	convert_previous_settings(ignore)
 
 if __name__ == "__main__":
 	log('entered LazyTV')
