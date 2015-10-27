4d79925f57bfef1ea058d4dc455194f65b19de71
KODeKarnage <sublime.kev@gmail.com>
2015-10-27 18:47:44 +1100

    fixed various errors in lazy player, including better handling for missing entries in the raw_details

--- resources/lib/lazy_player.py
+++ resources/lib/lazy_player.py
@@ -37,35 +37,57 @@ class LazyPlayer(xbmc.Player):
 
 			if video_type in ['unknown','episode']:
 
-				showid    = int(raw_details.get('tvshowid', 'none'))
-				epid      = int(raw_details.get('id', 'none'))
+				showid    = raw_details.get('tvshowid', 'none')
+				epid      = raw_details.get('id', 'none')
 
-				if showid != 'none' and epid != 'none':
+				try:
+					showid = int(showid)
+					epid   = int(epid)
+				except:
 
-					if not raw_details.get('episode',0):
+					return # Either epid or showid is none
 
-						return
+				if 'episode' not in raw_details:
 
-					else:
+					return # There is no episode number
 
-						showtitle  		= raw_details.get('showtitle','')
-						episode_np 		= T.fix_SE(raw_details.get('episode'))
-						season_np  		= T.fix_SE(raw_details.get('season'))
-						# duration   		= raw_details.get('runtime','')
-						duration 		= T.runtime_converter(xbmc.getInfoLabel('Player.Duration'))
-						resume_details  = raw_details.get('resume',{})
+				else:
 
-						# allow_prev, show_npid, ep_id = iStream_fix(show_id, showtitle, episode, season) #FUNCTION: REPLACE ISTREAM FIX
+					episode_np 		= T.fix_SE(raw_details.get('episode'), None)
+					season_np  		= T.fix_SE(raw_details.get('season'),  None)
 
-						self.queue.put({'episode_is_playing': {'allow_prev': allow_prev, 'showid': showid, 'epid': epid, 'duration': duration, 'resume': resume_details}})
+					if episode_np is None or season_np is None: 
 
-						self.playing_episodes_showid = showid
-						self.playing_episodes_epid   = epid
+						return # vital information is missing, exiting here
+
+					showtitle  		= raw_details.get('showtitle','')
+					# duration   	= raw_details.get('runtime','')
+					duration 		= T.runtime_converter(xbmc.getInfoLabel('Player.Duration'))
+					resume_details  = raw_details.get('resume',{})
+
+					# allow_prev, show_npid, ep_id = iStream_fix(show_id, showtitle, episode, season) #FUNCTION: REPLACE ISTREAM FIX
+
+					self.queue.put(	{'episode_is_playing': {
+															'allow_prev': allow_prev,
+															'showid'    : showid,
+															'epid'      : epid,
+															'duration'  : duration,
+															'resume'    : resume_details
+															}
+									})
+
+					# store the showid and epid locally
+					self.playing_episodes_showid = showid
+					self.playing_episodes_epid   = epid
 
 			elif video_type == 'movie':
 				# a movie might be playing in the random player, send the details to MAIN
 
-				self.queue.put({'movie_is_playing': {'movieid': movieid}})
+				movieid = raw_details.get('movieid', None)
+
+				if movieid is not None:
+
+					self.queue.put({'movie_is_playing': {'movieid': movieid}})
 
 
 	def onPlayBackStopped(self):
@@ -74,8 +96,16 @@ class LazyPlayer(xbmc.Player):
 
 
 	def onPlayBackEnded(self):
+		''' Send notification back to Main that the episode has ended. '''
 
-		self.queue.put({'player_has_ended': {'ended_showid': self.playing_episodes_showid, 'ended_epid': self.playing_episodes_epid}})
+		if all([self.playing_episodes_showid is not None, self.playing_episodes_epid is not None]):
+
+			self.queue.put({'player_has_ended': 
+											{
+											'ended_showid': self.playing_episodes_showid,
+											'ended_epid'  : self.playing_episodes_epid
+											}
+							})
 
 		self.playing_episodes_showid = None
 		self.playing_episodes_epid   = None
